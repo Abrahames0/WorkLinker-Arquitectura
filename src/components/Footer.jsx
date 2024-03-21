055334f0a16336b9c65b3b76a5f1858b25b51c37
Abrahames <abtaham0212@gmail.com>
2024-03-21 03:04:40 -0600

    mejoras SEO

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,51 +1,74 @@
-import * as React from 'react';
+import React from 'react';
 import { Box, Container, Grid, Typography, Link } from '@mui/material';
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import { useColorMode } from '@chakra-ui/react';
 import logo from '../landing/assets/img/WorkLinkerRecortada.png';
 
-export default function Footer() {
+const Footer = () => {
   const { colorMode } = useColorMode();
+  const isDarkMode = colorMode === 'dark';
 
-  const bgColor = colorMode === 'dark' ? 'grey.900' : 'background.paper';
-  const textColor = colorMode === 'dark' ? 'grey.300' : 'text.secondary';
+  const styles = {
+    footer: {
+      bgcolor: isDarkMode ? 'grey.900' : 'background.paper',
+      py: 6,
+    },
+    logo: {
+      height: '70px',
+    },
+    link: {
+      display: 'block',
+      color: isDarkMode ? 'grey.300' : 'text.secondary',
+    },
+    iconLink: {
+      display: 'flex',
+      alignItems: 'center',
+      gap: 1,
+    },
+    text: {
+      color: isDarkMode ? 'grey.300' : 'text.secondary',
+    },
+  };
 
   return (
-    <Box component="footer" sx={{ bgcolor: bgColor, py: 6 }}>
+    <Box component="footer" sx={styles.footer}>
       <Container maxWidth="lg">
         <Grid container spacing={4} justifyContent="space-between" alignItems="center">
           <Grid item xs={12} md={4}>
             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2 }}>
-              <img src={logo} alt="Logo de Web Linker Store" style={{ height: '70px' }} />
-              <Typography variant="subtitle1" color={textColor}>
+              <img src={logo} alt="Logo de Web Linker Store" style={styles.logo} />
+              <Typography variant="subtitle1" sx={styles.text}>
                 © {new Date().getFullYear()} Todos los derechos reservados, Página con fines educativos.
               </Typography>
             </Box>
           </Grid>
+
           <Grid item xs={12} md={2}>
-            <Typography variant="h6" color={textColor} gutterBottom>
+            <Typography variant="h6" sx={styles.text} gutterBottom>
               Recursos
             </Typography>
-            <Link href="/mapa-del-sitio" color={textColor} sx={{ display: 'block' }}>Mapa del Sitio</Link>
-            <Link href="/login-repartidores" color={textColor} sx={{ display: 'block' }}>Repartidor</Link>
-            <Link href="/login-empresa" color={textColor} sx={{ display: 'block' }}>Vender</Link>
+            <Link href="/mapa-del-sitio" sx={styles.link}>Mapa del Sitio</Link>
+            <Link href="/login-repartidores" sx={styles.link}>Repartidor</Link>
+            <Link href="/login-empresa" sx={styles.link}>Vender</Link>
           </Grid>
+
           <Grid item xs={12} md={2}>
-            <Typography variant="h6" color={textColor} gutterBottom>
+            <Typography variant="h6" sx={styles.text} gutterBottom>
               Legal
             </Typography>
-            <Link href="/privacy-policy" color={textColor} sx={{ display: 'block' }} target="_blank">Políticas de Privacidad</Link>
-            <Link href="/terms-conditions" color={textColor} sx={{ display: 'block' }} target="_blank">Términos & Condiciones</Link>
+            <Link href="/privacy-policy" sx={styles.link} target="_blank">Políticas de Privacidad</Link>
+            <Link href="/terms-conditions" sx={styles.link} target="_blank">Términos & Condiciones</Link>
           </Grid>
+
           <Grid item xs={12} md={2}>
-            <Typography variant="h6" color={textColor} gutterBottom>
+            <Typography variant="h6" sx={styles.text} gutterBottom>
               Contacto
             </Typography>
-            <Link href="mailto:noreplyworklinker@gmail.com" color={textColor} sx={{ display: 'block' }}>
+            <Link href="mailto:noreplyworklinker@gmail.com" sx={styles.link}>
               noreplyworklinker@gmail.com
             </Link>
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-              <Link href="https://www.facebook.com/profile.php?id=61557008636040&mibextid=LQQJ4d" aria-label="Facebook" rel="noopener noreferrer" target="_blank">
+            <Box sx={styles.iconLink}>
+            <Link href="https://www.facebook.com/profile.php?id=61557008636040&mibextid=LQQJ4d" aria-label="Facebook" rel="noopener noreferrer" target="_blank">
                 <FaFacebook size={25} />
               </Link>
               <Link href="https://www.instagram.com/xonixtechnology?igsh=NWR4ZnZuaTl1bnVv&utm_source=qr" aria-label="Instagram" rel="noopener noreferrer" target="_blank">
@@ -57,4 +80,6 @@ export default function Footer() {
       </Container>
     </Box>
   );
-}
+};
+
+export default Footer;
--- src/components/Inicio/inicio-bienvenida/MapaDelSitio.jsx
+++ src/components/Inicio/inicio-bienvenida/MapaDelSitio.jsx
@@ -5,96 +5,82 @@ import NavegacionLegal from './NavegacionLegal';
 import { useColorModeValue } from '@chakra-ui/react';
 import DynamicBreadcrumbs from '../../componentesRecicables/MigasDePan';
 
-function MapaDelSitio() {
-  const linkStyle = useColorModeValue({ color: '#000' }, { color: '#fff' });
+const MapaDelSitio = () => {
+  const linkStyle = useColorModeValue({ textDecoration: 'none', color: '#5d4fc6' }, { textDecoration: 'none', color: '#5d4fc6' });
 
   return (
     <>
-    <NavegacionLegal/>
-    <DynamicBreadcrumbs/>
-    <Box sx={{ textAlign: 'center', my: 4 }}>
-      <Box sx={{ display: 'inline-block' }}>
-         <img src={Logo} alt="Logo" style={{ maxWidth: '250px', marginBottom: '40px' }} />
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          <Link to="/mapa-del-sitio" aria-label="Mapa del Sitio" style={linkStyle}>
-          Mapa del Sitio
-          </Link>
-        </Typography>
+      <NavegacionLegal />
+      <DynamicBreadcrumbs />
+      <Box sx={{ textAlign: 'center', my: 4 }}>
+        <Box sx={{ display: 'inline-block' }}>
+          <img src={Logo} alt="WorkLinker Logo" style={{ maxWidth: '250px', marginBottom: '40px' }} />
+          <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+            <Link to="/mapa-del-sitio" aria-label="Mapa del Sitio">
+              Mapa del Sitio
+            </Link>
+          </Typography>
+        </Box>
+        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center' }}>
+          {/* Sección para Usuarios */}
+          <CardSection title="Usuarios" items={[
+            { to: "/login-users", label: "Login Usuarios" },
+            { to: "/usuario-compras", label: "Detalles de Compras" },
+            { to: "/pago-tarjeta", label: "Pago con Tarjeta de Crédito" },
+            { to: "/inicio-usuarios/carrito", label: "Carrito de Compras" },
+            { to: "/perfil-usuario", label: "Perfil de Usuario" },
+            { to: "/registro-Usuario", label: "Registro de Usuario" },
+            { to: "/inicio-usuarios", label: "Inicio Usuarios" }
+          ]} linkStyle={linkStyle} />
+
+          {/* Sección para Vender */}
+          <CardSection title="Vender" items={[
+            { to: "/login-empresa", label: "Login Empresa" },
+            { to: "/registro-empresa", label: "Registro de Empresa" },
+            { to: "/agregar-producto", label: "Registro de Producto" },
+            { to: "/perfil-proveedor", label: "Perfil de Proveedor" },
+            { to: "/inicio-empresa", label: "Inicio Empresa Proveedor" }
+          ]} linkStyle={linkStyle} />
+
+          {/* Sección para Repartidores */}
+          <CardSection title="Repartidores" items={[
+            { to: "/login-repartidores", label: "Login Repartidores" },
+            { to: "/inicio-repartidores", label: "Inicio Repartidores" },
+            { to: "/registro-repartidor", label: "Registro Repartidor" },
+            { to: "/perfil-repartidor", label: "Perfil Repartidor" }
+          ]} linkStyle={linkStyle} />
+
+          {/* Sección General */}
+          <CardSection title="General" items={[
+            { to: "/privacy-policy", label: "Política de Privacidad" },
+            { to: "/terms-conditions", label: "Términos y Condiciones" }
+          ]} linkStyle={linkStyle} />
+        </div>
       </Box>
-    <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center' }}>
-      <section aria-labelledby="usuarios-heading" style={{ flexBasis: '30%', maxWidth: '400px', margin: '10px' }}>
-        <Card sx={{ backgroundColor: '#f5f5f5', boxShadow: '0 2px 5px rgba(0,0,0,0.1)' }}>
-          <CardContent>
-            <Typography id="usuarios-heading" variant="h5" component="h2">Usuarios</Typography>
-            <nav>
-              <List sx={{ padding: '0 20px' }}>
-                <ListItem>
-                  <Link to="/login-users" aria-label="Login de Usuarios" style={{ textDecoration: 'none', color: '#5d4fc6' }}>
-                    Login Usuarios
-                  </Link>
-                </ListItem>
-                <ListItem><Link to="/usuario-compras" aria-label="Detalles de Compras" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Detalles de Compras</Link></ListItem>
-              	<ListItem><Link to="/pago-tarjeta" aria-label="Pago con Tarjeta de Crédito" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Pago con Tarjeta de Crédito</Link></ListItem>
-                <ListItem><Link to="/inicio-usuarios/carrito" aria-label="Carrito de Compras" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Carrito de Compras</Link></ListItem>
-                <ListItem><Link to="/perfil-usuario" aria-label="Perfil de Usuario" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Perfil de Usuario</Link></ListItem>
-                <ListItem><Link to="/registro-Usuario" aria-label="Registro de Usuario" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Registro de Usuario</Link></ListItem>
-                <ListItem> <Link to="/inicio-usuarios" aria-label="Inicio Usuarios" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Inicio Usuarios</Link></ListItem>
-              </List>
-            </nav>
-          </CardContent>
-        </Card>
-      </section>
-
-      <section aria-labelledby="empresa-heading" style={{ flexBasis: '30%', maxWidth: '400px', margin: '10px' }}>
-        <Card sx={{ backgroundColor: '#f5f5f5', boxShadow: '0 2px 5px rgba(0,0,0,0.1)' }}>
-          <CardContent>
-            <Typography id="empresa-heading" variant="h5" component="h2">Vender</Typography>
-            <nav>
-              <List sx={{ padding: '0 20px' }}>
-              <ListItem><Link to="/login-empresa" aria-label="Login Empresa" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Login Empresa</Link></ListItem>
-                <ListItem><Link to="/registro-empresa" aria-label="Registro de Empresa" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Registro de Empresa</Link></ListItem>
-                <ListItem><Link to="/agregar-producto" aria-label="Registro de Producto" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Registro de Producto</Link></ListItem>
-                <ListItem><Link to="/perfil-proveedor" aria-label="Perfil de Proveedor" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Perfil de Proveedor</Link></ListItem>
-                <ListItem><Link to="/inicio-empresa" aria-label="Inicio Empresa Proveedor" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Inicio Empresa Proveedor</Link></ListItem>
-              </List>
-            </nav>
-          </CardContent>
-        </Card>
-      </section>
-
-      <section aria-labelledby="repartidores-heading" style={{ flexBasis: '30%', maxWidth: '400px', margin: '10px' }}>
-        <Card sx={{ backgroundColor: '#f5f5f5', boxShadow: '0 2px 5px rgba(0,0,0,0.1)' }}>
-          <CardContent>
-            <Typography id="repartidores-heading" variant="h5" component="h2">Repartidores</Typography>
-            <nav>
-              <List sx={{ padding: '0 20px' }}>
-                <ListItem><Link to="/login-repartidores" aria-label="Login Repartidores" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Login Repartidores</Link></ListItem>
-                <ListItem><Link to="/inicio-repartidores" aria-label="Inicio Repartidores" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Inicio Repartidores</Link></ListItem>
-                <ListItem><Link to="/registro-repartidor" aria-label="Registro Repartidor" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Registro Repartidor</Link></ListItem>
-                <ListItem><Link to="/perfil-repartidor" aria-label="Perfil Repartidor" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Perfil Repartidor</Link></ListItem>
-              </List>
-            </nav>
-          </CardContent>
-        </Card>
-      </section>
-
-      <section aria-labelledby="general-heading" style={{ flexBasis: '30%', maxWidth: '400px', margin: '10px' }}>
-        <Card sx={{ backgroundColor: '#f5f5f5', boxShadow: '0 2px 5px rgba(0,0,0,0.1)' }}>
-          <CardContent>
-            <Typography id="general-heading" variant="h5" component="h2">General</Typography>
-            <nav>
-              <List sx={{ padding: '0 20px' }}>
-                <ListItem><Link to="/privacy-policy" aria-label="Política de Privacidad" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Política de Privacidad</Link></ListItem>
-                 <ListItem><Link to="/terms-conditions" aria-label="Términos y Condiciones" style={{ textDecoration: 'none', color: '#5d4fc6' }}>Términos y Condiciones</Link></ListItem>
-              </List>
-            </nav>
-          </CardContent>
-        </Card>
-      </section>
-    </div>
-    </Box>
     </>
   );
-}
+};
+
+// Componente para secciones de tarjetas
+const CardSection = ({ title, items, linkStyle }) => (
+  <section style={{ flexBasis: '30%', maxWidth: '400px', margin: '10px' }}>
+    <Card sx={{ backgroundColor: '#f5f5f5', boxShadow: '0 2px 5px rgba(0,0,0,0.1)' }}>
+      <CardContent>
+        <Typography variant="h5" component="h2">{title}</Typography>
+        <nav>
+          <List sx={{ padding: '0 20px'}}>
+            {items.map((item, index) => (
+              <ListItem key={index}>
+                <Link to={item.to} aria-label={item.label} style={linkStyle}>
+                  {item.label}
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+        </nav>
+      </CardContent>
+    </Card>
+  </section>
+);
 
 export default MapaDelSitio;
